5f773fcd7fed9e189f2bf27ece1e75e3a502ddf2
Sergio Lema <sergio.lema@ekino.fr>
2019-11-18 16:18:42 +0100

    Use the index with the most elements

--- app/services/search_service.py
+++ app/services/search_service.py
@@ -21,11 +21,15 @@ class SearchService(object):
         return self.find_in_docs(docs, search_context)
 
     def find_field_in_index(self, col_meta_data, search_context):
+        best_indexed_value = None
         for indexed_value in search_context.filter_keys:
             k = list(indexed_value.keys())[0]
             if k in col_meta_data.indexes:
-                return indexed_value
-        return None
+                if best_indexed_value == None:
+                    best_indexed_value = indexed_value
+                elif col_meta_data.indexes[best_indexed_value] < col_meta_data.indexes[indexed_value]:
+                    best_indexed_value = indexed_value
+        return best_indexed_value
 
     def find_in_docs(self, docs, search_context):
             results = []
